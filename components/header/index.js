0add06838532aa150f0e5dd22c237f78891b3613
Riad Benguella <benguella@gmail.com>
2021-07-29 14:04:23 +0100

    Fix typo

--- components/header/index.js
+++ components/header/index.js
@@ -5,7 +5,7 @@ export default function Header({ children }) {
         <div className='w-full flex items-center justify-between border-b border-wordpress lg:border-none'>
           <div className='flex items-center'>
             <a className='flex items-center text-base font-medium text-white hover:text-indigo-50 py-5' href='#'>
-              <span className='ml-5'>Is Gutenberg Fast Yest?</span>
+              <span className='ml-5'>Is Gutenberg Fast Yet?</span>
             </a>
 
             <div className='ml-10 p6'>{children}</div>
