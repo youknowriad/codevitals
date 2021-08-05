6677a916392173814c23b785498422c11a5b5da7
Riad Benguella <benguella@gmail.com>
2021-08-05 09:11:43 +0100

    fill missing base values

--- pages/api/log.js
+++ pages/api/log.js
@@ -17,43 +17,41 @@ export default async (req, res) => {
           throw 'Invalid token'
         }
         const [availableMetrics] = await conn.query('select * from metric where project_id = ?', project.id)
-        const [[{ c: count }]] = await conn.query('select count(*) as c from perf where project_id = ?', project.id)
 
-        // No base yet, fill base values.
-        if (count === 0) {
-          const baseTuples = []
-          Object.entries(baseMetrics).forEach(([metricKey, metricValue]) => {
-            const availableMetric = availableMetrics.find((m) => m.key === metricKey)
-            if (availableMetric) {
-              baseTuples.push([project.id, baseHash, baseHash, availableMetric.id, metricValue, new Date(timestamp)])
-            }
-          })
-
-          for (const tuple of baseTuples) {
-            await conn.query(
-              'insert into perf (project_id, branch, hash, metric_id, value, measured_at) values (?, ?,?,?,?,?)',
-              tuple
-            )
-          }
-        }
-
-        // Insert and normalize metrics
+        // No base perfs yet, fill base values.
         const [basePerfs] = await conn.query('select * from perf where hash = ? and project_id = ?', [
           baseHash,
           project.id
         ])
+        const baseTuples = []
+        Object.entries(baseMetrics).forEach(([metricKey, metricValue]) => {
+          const availableMetric = availableMetrics.find((m) => m.key === metricKey)
+          const baseValue = availableMetric ? basePerfs.find((p) => p.metric_id === availableMetric.id) : null
+          if (availableMetric && !baseValue) {
+            baseTuples.push([project.id, baseHash, baseHash, availableMetric.id, metricValue, new Date(timestamp)])
+          }
+        })
+
+        for (const tuple of baseTuples) {
+          await conn.query(
+            'insert into perf (project_id, branch, hash, metric_id, value, measured_at) values (?,?,?,?,?,?)',
+            tuple
+          )
+        }
+
+        // Insert and normalize metrics
         const tuples = []
         Object.entries(metrics).forEach(([metricKey, metricValue]) => {
           const availableMetric = availableMetrics.find((m) => m.key === metricKey)
           const baseValue = availableMetric ? basePerfs.find((p) => p.metric_id === availableMetric.id) : null
 
-          if (availableMetric && baseValue) {
+          if (availableMetric) {
             tuples.push([
               project.id,
               branch,
               hash,
               availableMetric.id,
-              (metricValue * baseValue.value) / baseMetrics[availableMetric.key],
+              baseValue ? (metricValue * baseValue.value) / baseMetrics[availableMetric.key] : metricValue,
               new Date(timestamp)
             ])
           }
