c5c5ab6b7b00647a04ed399d9db7e1b53b81b216
Riad Benguella <benguella@gmail.com>
2022-03-22 13:52:44 +0100

    More UI tweaks

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Line } from 'react-chartjs-2'
 import { useState } from 'react'
 import { ArrowSmDownIcon, ArrowSmUpIcon, InformationCircleIcon } from '@heroicons/react/solid'
 import Header from '../components/header'
+import Spinner from '../components/spinner'
 
 const formatNumber = (number) => number.toLocaleString(undefined, { maximumFractionDigits: 2 })
 const fetcher = (url) => fetch(url).then((res) => res.json())
@@ -18,87 +19,88 @@ function Metric({ metric }) {
 
   return (
     <div className='m-4'>
-      {perf?.length > 0 && (
-        <>
-          <div>
-            <div className='sm:hidden'>
-              <label htmlFor='tabs' className='sr-only'>
-                Select a limit
-              </label>
-              {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-              <select
-                id='tabs'
-                name='tabs'
-                className='block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
-                defaultValue={200}
-                onChange={(event) => setLimit(parseInt(event.target.value))}
-              >
-                {limits.map((limit) => (
-                  <option key={limit.value}>{limit.label}</option>
-                ))}
-              </select>
-            </div>
-            <div className='hidden sm:block'>
-              <div className='border-b border-gray-200'>
-                <nav className='-mb-px flex space-x-8' aria-label='Tabs'>
-                  {limits.map((limit) => (
-                    <button
-                      key={limit.value}
-                      className={classnames(
-                        limit.value === currentLimit
-                          ? 'border-indigo-500 text-indigo-600'
-                          : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                        'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
-                      )}
-                      aria-current={limit.value === currentLimit ? 'page' : undefined}
-                      onClick={() => setLimit(limit.value)}
-                    >
-                      {limit.label}
-                    </button>
-                  ))}
-                </nav>
-              </div>
-            </div>
+      <div>
+        <div className='sm:hidden'>
+          <label htmlFor='tabs' className='sr-only'>
+            Select a limit
+          </label>
+          {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+          <select
+            id='tabs'
+            name='tabs'
+            className='block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
+            defaultValue={200}
+            onChange={(event) => setLimit(parseInt(event.target.value))}
+          >
+            {limits.map((limit) => (
+              <option key={limit.value}>{limit.label}</option>
+            ))}
+          </select>
+        </div>
+        <div className='hidden sm:block'>
+          <div className='border-b border-gray-200'>
+            <nav className='-mb-px flex space-x-8' aria-label='Tabs'>
+              {limits.map((limit) => (
+                <button
+                  key={limit.value}
+                  className={classnames(
+                    limit.value === currentLimit
+                      ? 'border-indigo-500 text-indigo-600'
+                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                    'flex gap-2 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
+                  )}
+                  aria-current={limit.value === currentLimit ? 'page' : undefined}
+                  onClick={() => setLimit(limit.value)}
+                >
+                  {limit.label}
+                  {!perf?.length && limit.value === currentLimit && <Spinner />}
+                </button>
+              ))}
+            </nav>
           </div>
-          <div className='m-8'>
-            <Line
-              data={{
-                labels: perf.map((p) => p.hash),
-                datasets: [
-                  {
-                    label: metric.name,
-                    data: perf.map((p) => p.value)
-                  }
-                ]
-              }}
-              options={{
-                responsive: true,
-                scales: {
-                  x: {
-                    display: false
-                  },
-                  y: {
-                    min: 0
-                  }
+        </div>
+      </div>
+      <div className='m-8'>
+        {!!perf?.length && (
+          <Line
+            data={{
+              labels: perf.map((p) => p.hash),
+              datasets: [
+                {
+                  label: metric.name,
+                  data: perf.map((p) => p.value)
                 }
-              }}
-            />
-            <div className='mt-4 rounded-md bg-blue-50 p-4'>
-              <div className='flex'>
-                <div className='flex-shrink-0'>
-                  <InformationCircleIcon className='h-5 w-5 text-blue-400' aria-hidden='true' />
-                </div>
-                <div className='ml-3 flex-1 md:flex md:justify-between'>
-                  <p className='text-sm text-blue-700'>
-                    For more stability, the following numbers compare the evolution of the metrics by computing average
-                    of 20 sequential values.
-                  </p>
-                </div>
+              ]
+            }}
+            options={{
+              responsive: true,
+              scales: {
+                x: {
+                  display: false
+                },
+                y: {
+                  min: 0
+                }
+              }
+            }}
+          />
+        )}
+        {!!perf?.length && (
+          <div className='mt-4 rounded-md bg-blue-50 p-4'>
+            <div className='flex'>
+              <div className='flex-shrink-0'>
+                <InformationCircleIcon className='h-5 w-5 text-blue-400' aria-hidden='true' />
+              </div>
+              <div className='ml-3 flex-1 md:flex md:justify-between'>
+                <p className='text-sm text-blue-700'>
+                  For more stability, the following numbers compare the evolution of the metrics by computing average of
+                  20 sequential values.
+                </p>
               </div>
             </div>
           </div>
-        </>
-      )}
+        )}
+      </div>
     </div>
   )
 }
@@ -120,7 +122,20 @@ function MetricCard({ metric, onSelect }) {
       className='px-4 py-5 bg-gray-50 shadow rounded-sm overflow-hidden sm:p-6 pointer hover:bg-gray-100'
     >
       <dt className='text-base font-normal text-gray-900'>{metric.name}</dt>
-      {!data && 'Loading...'}
+      {!data && (
+        <div className='mt-5 mb-2 animate-pulse grid grid-cols-6 gap-3'>
+          <div className='col-span-1'>
+            <div className='h-2 bg-gray-500 rounded' />
+          </div>
+          <div className='col-span-1'>
+            <div className='h-2 bg-gray-500 rounded' />
+          </div>
+          <div className='col-span-3'></div>
+          <div className='col-span-1'>
+            <div className='h-2 bg-gray-500 rounded' />
+          </div>
+        </div>
+      )}
       {data && (
         <dd className='mt-1 flex justify-between items-baseline md:block lg:flex'>
           {data?.average && (
@@ -184,22 +199,26 @@ export default function Home() {
 
   return (
     <>
-      <Header />
-      <div>
-        {!metrics && <div className='w-full h-full flex flex-col items-center justify-center text-lg'>Loading...</div>}
-        {metrics && !metrics.length && (
-          <div className='w-full h-full flex flex-col items-center justify-center text-lg'>No data available.</div>
-        )}
-        {metrics && metrics.length && (
-          <div className='px-4'>
-            <dl className='mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3'>
-              {metrics.map((metric) => (
-                <MetricCard key={metric.id} metric={metric} onSelect={() => setSelectedMetric(metric)} />
-              ))}
-            </dl>
-          </div>
-        )}
-        {displayedMetric && <Metric metric={displayedMetric || metrics?.[0]} />}
+      <div className='h-screen flex flex-col'>
+        <div>
+          <Header />
+        </div>
+        <div className='flex-1'>
+          {!metrics && <div className='w-full h-full flex flex-col items-center justify-center text-lg'>Loading...</div>}
+          {metrics && !metrics.length && (
+            <div className='w-full flex flex-col items-center justify-center text-lg'>No data available.</div>
+          )}
+          {metrics && metrics.length && (
+            <div className='px-4'>
+              <dl className='mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3'>
+                {metrics.map((metric) => (
+                  <MetricCard key={metric.id} metric={metric} onSelect={() => setSelectedMetric(metric)} />
+                ))}
+              </dl>
+            </div>
+          )}
+          {displayedMetric && <Metric metric={displayedMetric || metrics?.[0]} />}
+        </div>
       </div>
     </>
   )
