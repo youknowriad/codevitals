37b2ffb5955094c89d64287317df6c032a1ffb6a
Riad Benguella <benguella@gmail.com>
2022-03-22 19:28:42 +0100

    Fix order

--- pages/api/evolution/[metric_id].js
+++ pages/api/evolution/[metric_id].js
@@ -7,10 +7,11 @@ export default async (req, res) => {
   const { metric_id, limit } = req.query
   const parsedLimit = parseInt(limit)
   const [perfs] = await conn.query(
-    'select * from perf where branch = "trunk" and metric_id = ? order by measured_at ASC' +
+    'select * from perf where branch = "trunk" and metric_id = ? order by measured_at DESC' +
       (parsedLimit && parsedLimit !== '0' ? ' limit ' + parsedLimit : ''),
     metric_id
   )
+  perfs.reverse()
   res.statusCode = 200
   res.json(perfs)
 }
