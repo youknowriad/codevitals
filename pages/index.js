7cefa07d0093554ceee866d67000828e7e91577a
Riad Benguella <benguella@gmail.com>
2022-03-24 18:16:58 +0100

    Remove useless message

--- pages/index.js
+++ pages/index.js
@@ -85,21 +85,6 @@ function Metric({ metric }) {
             }}
           />
         )}
-        {!!perf?.length && (
-          <div className='mt-4 rounded-md bg-blue-50 p-4'>
-            <div className='flex'>
-              <div className='flex-shrink-0'>
-                <InformationCircleIcon className='h-5 w-5 text-blue-400' aria-hidden='true' />
-              </div>
-              <div className='ml-3 flex-1 md:flex md:justify-between'>
-                <p className='text-sm text-blue-700'>
-                  For more stability, the following numbers compare the evolution of the metrics by computing average of
-                  20 sequential values.
-                </p>
-              </div>
-            </div>
-          </div>
-        )}
       </div>
     </div>
   )
