5fce1f36a9e92be1e709ec949a107eeca0a8a4bc
Riad Benguella <benguella@gmail.com>
2023-12-10 17:33:57 +0100

    Add speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import 'tailwindcss/tailwind.css'
 import { SessionProvider } from 'next-auth/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <SpeedInsights />
     </SessionProvider>
   )
 }
