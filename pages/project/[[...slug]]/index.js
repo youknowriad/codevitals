d6c2a2026fbc021d21c99c94d0e074016252b18b
Pascal Birchler <pascal.birchler@gmail.com>
2025-01-09 14:32:59 +0100

    Add metrics selection listbox (#32)

--- pages/project/[[...slug]]/index.js
+++ pages/project/[[...slug]]/index.js
@@ -2,7 +2,9 @@ import classnames from 'classnames'
 import useSWR from 'swr'
 import { Line } from 'react-chartjs-2'
 import { useState, useEffect, useRef, useCallback, useMemo } from 'react'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
 import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid'
+import { CheckIcon } from '@heroicons/react/20/solid'
 import Layout from '../../../components/layout'
 import { Chart as ChartJS, Tooltip, registerables } from 'chart.js'
 import dynamic from 'next/dynamic'
@@ -323,8 +325,11 @@ function Metrics({ id, repository }) {
   const router = useRouter()
   const [projectSlug, metricSlug] = router.query.slug
   const { data: metrics } = useSWR('/api/metrics/' + id, fetcher)
+  const defaultMetrics = metrics?.filter((metric) => metric.default_visible !== 0)
   const metricFromParam = metrics?.find((metric) => metric.key === metricSlug)
   const displayedMetric = metricFromParam || metrics?.[0]
+
+  const [selectedMetrics, setSelectedMetrics] = useState(defaultMetrics)
 
   useEffect(() => {
     // Set the metric slug in the URL if it's not already set.
@@ -332,6 +337,8 @@ function Metrics({ id, repository }) {
       router.push(`/project/${projectSlug}/${displayedMetric.key}`)
     }
   }, [metricSlug, displayedMetric])
+
+  const _selectedMetrics = selectedMetrics || defaultMetrics
 
   return (
     <>
@@ -342,16 +349,41 @@ function Metrics({ id, repository }) {
           <div className='w-full h-full flex flex-col items-center justify-center text-lg'>No data available.</div>
         )}
         {!!metrics?.length && (
-          <dl className='grid grid-cols-1 sm:grid-cols-5 bg-gray-50 border-b border-gray-200'>
-            {metrics.map((metric) => (
-              <MetricCard
-                key={metric.id}
-                metric={metric}
-                onSelect={() => router.push(`/project/${projectSlug}/${metric.key}`, undefined, { scroll: false })}
-                isActive={displayedMetric === metric}
-              />
-            ))}
-          </dl>
+          <>
+            <dl className='grid grid-cols-1 sm:grid-cols-5 bg-gray-50 border-b border-gray-200'>
+              <Listbox value={_selectedMetrics} onChange={setSelectedMetrics} multiple>
+                <ListboxButton
+                  className='px-4 py-5 overflow-hidden sm:p-6 pointer hover:bg-blue-200 border-b border-r border-gray-200 border-t bg-blue-100'
+                  style={{ marginBottom: -1 }}
+                >
+                  Visible Metrics <strong>({(selectedMetrics || defaultMetrics).length})</strong>
+                </ListboxButton>
+                <ListboxOptions
+                  anchor='bottom start'
+                  className='absolute right-0 z-10 w-80 h-80 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
+                >
+                  {metrics.map((metric) => (
+                    <ListboxOption
+                      key={metric.id}
+                      value={metric}
+                      className='group flex gap-2 px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'
+                    >
+                      <CheckIcon className='invisible size-5 group-data-[selected]:visible' />
+                      {metric.name}
+                    </ListboxOption>
+                  ))}
+                </ListboxOptions>
+              </Listbox>
+              {(selectedMetrics || defaultMetrics).map((metric) => (
+                <MetricCard
+                  key={metric.id}
+                  metric={metric}
+                  onSelect={() => router.push(`/project/${projectSlug}/${metric.key}`, undefined, { scroll: false })}
+                  isActive={displayedMetric === metric}
+                />
+              ))}
+            </dl>
+          </>
         )}
         {displayedMetric && <Metric metric={displayedMetric} repository={repository} />}
       </Layout>
